f56572be2f55bce20bca89c53544a093f1b4623f
Edge Quintanilla <equintanilla@discoveryed.com>
2025-09-29 12:52:17 -0500

    astroids~FPS locked

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -19,6 +21,7 @@ def main():
 
         screen.fill(color="black")
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
 
 
 if __name__ == "__main__":
